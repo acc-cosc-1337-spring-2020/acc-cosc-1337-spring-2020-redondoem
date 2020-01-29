9fe1e85695142b79218bc62125fc66d5fe1be467
redondoem <esthermaryrose.jadol@g.austincc.edu>
2020-01-28 19:23:39 -0600

    multiply and display numbers

--- test/homework_test/01_expressions_test/01_expressions_tests.cpp
+++ test/homework_test/01_expressions_test/01_expressions_tests.cpp
@@ -12,3 +12,9 @@ TEST_CASE("Verify sum_numbers function")
 	REQUIRE(add_numbers(10, 10) == 20);
 }
 
+TEST_CASE("Verify multiply_numbers function")
+{
+	REQUIRE(multiply_numbers(2, 2) == 4);
+	REQUIRE(multiply_numbers(3, 3) == 9);
+	REQUIRE(multiply_numbers(5, 5) == 25);
+}
