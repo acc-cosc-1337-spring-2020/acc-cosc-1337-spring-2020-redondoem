2c4509a0775b260a0021dd6e2263e11ecc36df78
redondoem <esthermaryrose.jadol@g.austincc.edu>
2020-02-04 18:40:02 -0600

    bool changes

--- src/examples/02_module/01_if/main.cpp
+++ src/examples/02_module/01_if/main.cpp
@@ -8,15 +8,28 @@ int main()
 {
 	//create a bool variable named even
 	//create an int variable named value
+	bool even;
+	int value;
 
 	//prompt user for a keyboard number
 	//assign the number to the value variable
+	cout << "Type a number: ";
+	cin >> value;
 
 	//call the is_even function with value as its argument
 	//and assign its return value to the even variable
-	
+	even = is_even(value);
+
 
 	//display even if number is even
+	if (even == true)
+	{
+		cout << value << " is even.";
+	}
+	else
+	{
+		cout << value << " is odd.";
+	}
 
 
 	return 0;
