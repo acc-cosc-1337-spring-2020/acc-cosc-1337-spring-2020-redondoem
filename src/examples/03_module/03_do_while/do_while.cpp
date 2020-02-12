30dbed0ac082e5dd98f8b87436a0d74b51226167
redondoem <esthermaryrose.jadol@g.austincc.edu>
2020-02-11 18:58:51 -0600

    do whileeeee

--- src/examples/03_module/03_do_while/do_while.cpp
+++ src/examples/03_module/03_do_while/do_while.cpp
@@ -1,8 +1,27 @@
 #include "do_while.h"
+#include "while.h"
+#include <iostream>
+
+using std::cout; using std::cin;
 
 //Write code for void function prompt_user to loop until
 //user opts not to continue.  
+void prompt_user()
+{
+	int choice;
 
+	do
+	{
+		int num;
+		cout << "Enter a number: ";
+		cin >> num;
+		cout << sum_of_squares(num) << "\n\n";
+
+		std::cout << "Continue: ";
+		std::cin >> choice;
+
+	} while (choice == 1);
+}
 //Write code for function run_menu that prompts  user for a 
 //number from 1 to 4 and displays the option user selected.
 /*
