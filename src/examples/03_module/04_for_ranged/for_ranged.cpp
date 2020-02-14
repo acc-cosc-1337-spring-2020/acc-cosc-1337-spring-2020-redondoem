a9720a395008b01a55bcb942a1db325f47114a1c
redondoem <esthermaryrose.jadol@g.austincc.edu>
2020-02-13 18:49:05 -0600

    const, value changes

--- src/examples/03_module/04_for_ranged/for_ranged.cpp
+++ src/examples/03_module/04_for_ranged/for_ranged.cpp
@@ -16,7 +16,23 @@ s
 t
 */
 
+void loop_string_w_index(const std::string &str)
+{
+	for (int i = 0; i < str.length(); ++i)
+	{
+		std::cout << str[i] << "\n"; 
 
+	}
+
+}
+void loop_string_w_auto(const std::string & str)
+{
+	for (auto ch : str)
+	{
+		std::cout << ch << "\n";
+
+	}
+}
 /*
 Write code for loop_string_w_index that accepts a string parameter.
 The function uses a for ranged loop using auto to iterate and display the characters in the
--- src/examples/03_module/04_for_ranged/for_ranged.h
+++ src/examples/03_module/04_for_ranged/for_ranged.h
@@ -3,8 +3,10 @@
 
 //write a void function prototype for loop_string_w_index that accepts a string parameter
 
+void loop_string_w_index(const std::string &str);
 
 
 //write a void function prototype for loop_string_w_auto  that accepts a string parameter
 
+void loop_string_w_auto(const std:: string & str);
 
