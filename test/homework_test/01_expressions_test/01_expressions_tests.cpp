8387c3a9eabcec11139f12d5b93d38af389247a9
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-02-15 11:59:02 -0600

    restore homework tests cases

---
+++ test/homework_test/01_expressions_test/01_expressions_tests.cpp
@@ -0,0 +1,21 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+#include "expressions.h"
+
+TEST_CASE("Verify Test Configuration", "verification") {
+	REQUIRE(true == true);
+}
+
+TEST_CASE("Verify sum_numbers function") 
+{
+	REQUIRE(add_numbers(5, 5) == 10);
+	REQUIRE(add_numbers(10, 10) == 20);
+}
+
+TEST_CASE("Verify multiply_numbers function") 
+{
+	REQUIRE(multiply_numbers(2, 2) == 4);
+	REQUIRE(multiply_numbers(3, 3) == 9);
+	REQUIRE(multiply_numbers(5, 5) == 25);
+}
+
