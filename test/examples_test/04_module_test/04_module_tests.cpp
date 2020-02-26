8d4fd27b4c63704f6db35c2b5409c0c74578d12c
redondoem <esthermaryrose.jadol@g.austincc.edu>
2020-02-25 19:08:30 -0600

    private and public

--- test/examples_test/04_module_test/04_module_tests.cpp
+++ test/examples_test/04_module_test/04_module_tests.cpp
@@ -1,7 +1,41 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "bank_account.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Test BankAccount constructor")
+{
+	BankAccount account(500);
+
+	REQUIRE(account.get_balance() == 500);
+}
+
+TEST_CASE("Test Bank Account deposit")
+{
+	BankAccount account(500);
+	REQUIRE(account.get_balance() == 500);
+
+	account.deposit(50);
+	REQUIRE(account.get_balance() == 550);
+
+	account.deposit(-50);
+	REQUIRE(account.get_balance() == 550);
+
+}
+TEST_CASE("Test Bank Account withdraw")
+{
+	BankAccount account(500);
+	REQUIRE(account.get_balance() == 500);
+
+	account.withdraw(50);
+	REQUIRE(account.get_balance() == 450);
+
+	account.withdraw(-1);
+	REQUIRE(account.get_balance() == 450);
+
+	account.withdraw(451);
+	REQUIRE(account.get_balance() == 450); 
+}
