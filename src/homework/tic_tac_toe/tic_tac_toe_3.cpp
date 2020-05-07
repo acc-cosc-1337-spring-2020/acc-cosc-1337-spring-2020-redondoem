d6c4d6f5c3704f6919e775bbbe564ff1306b2a72
esthermaryrose.jadol <esthermaryrose.jadol@g.austincc.edu>
2020-05-07 17:58:52 -0500

    homework

--- src/homework/tic_tac_toe/tic_tac_toe_3.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe_3.cpp
@@ -32,19 +32,90 @@ Win diagonally
 
 bool Tic_tac_toe_3::check_column_win()
 {
-	if (game_over() == false);
+	if (pegs[0] == "X" && pegs[3] == "X" && pegs[6] == "X")
 	{
+		return true;
+	}
+	else if (pegs[0] == "O" && pegs[3] == "O" && pegs[6] == "O" )
+	{
+		return true;
 
+	}
+	else if (pegs[1] == "X" && pegs[4] == "X" && pegs[7] == "X" )
+	{
+		return true;
+	}
+	else if (pegs[1] == "O" && pegs[4] == "O" && pegs[7] == "O" )
+	{
+		return true;
+	}
+	else if (pegs[2] == "X" && pegs[5] == "X" && pegs[8] == "X" )
+	{
+		return true;
+	}
+	else if (pegs[2] == "O" && pegs[5] == "O" && pegs[8] == "O")
+	{
+		return true;
 	}
 	return false;
 }
 
 bool Tic_tac_toe_3::check_row_win()
 {
+	if (pegs[0] == "X" && pegs[1] == "X" && pegs[2] == "X")
+	{
+		return true;
+	}
+	else if (pegs[0] == "O" && pegs[1] == "O" && pegs[2] == "O")
+	{
+		return true;
+
+	}
+	else if (pegs[3] == "X" && pegs[4] == "X" && pegs[5] == "X")
+	{
+		return true;
+	}
+	else if (pegs[3] == "O" && pegs[4] == "O" && pegs[5] == "O")
+	{
+		return true;
+	}
+	else if (pegs[6] == "X" && pegs[7] == "X" && pegs[8] == "X")
+	{
+		return true;
+	}
+	else if (pegs[6] == "O" && pegs[7] == "O" && pegs[8] == "O")
+	{
+		return true;
+	}
 	return false;
 }
 
 bool Tic_tac_toe_3::check_diagonal_win()
 {
+	if (pegs[0] == "X" && pegs[1] == "X" && pegs[2] == "X")
+	{
+		return true;
+	}
+	else if (pegs[0] == "O" && pegs[1] == "O" && pegs[2] == "O")
+	{
+		return true;
+
+	}
+	else if (pegs[3] == "X" && pegs[4] == "X" && pegs[5] == "X")
+	{
+		return true;
+	}
+	else if (pegs[3] == "O" && pegs[4] == "O" && pegs[5] == "O")
+	{
+		return true;
+	}
+	else if (pegs[6] == "X" && pegs[7] == "X" && pegs[8] == "X")
+	{
+		return true;
+	}
+	else if (pegs[6] == "O" && pegs[7] == "O" && pegs[8] == "O")
+	{
+		return true;
+	}
 	return false;
 }
